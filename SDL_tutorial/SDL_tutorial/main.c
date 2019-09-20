db18365b63696b2c5a069369621a849309d8fb37
Brad Jennings <brad.jennings@cerner.com>
2019-09-20 10:03:21 -0500

    Basic SDL window in C

---
+++ SDL_tutorial/SDL_tutorial/main.c
@@ -0,0 +1,32 @@
+// cerner_2^5_2019
+#include <stdbool.h>
+#include <SDL.h>
+
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could notr initialize! SDL_Error: %s\n", SDL_GetError());
+    } else {
+        SDL_Window *window = window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+        if (window == NULL) {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        } else {
+            SDL_Surface *screenSurface = screenSurface = SDL_GetWindowSurface(window);
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+            SDL_UpdateWindowSurface(window);
+            
+            bool quit = false;
+            SDL_Event e;
+            while (!quit) {
+                while (SDL_PollEvent(&e) != 0) {
+                    if (e.type == SDL_QUIT) {
+                        quit = true;
+                    }
+                }
+            }
+            
+            SDL_DestroyWindow(window);
+        }
+    }
+    SDL_Quit();
+    return 0;
+}
