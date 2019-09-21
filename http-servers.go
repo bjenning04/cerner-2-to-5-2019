28d201e3c5621d1e391ddf1761710d799a0e4801
Brad Jennings <brad.jennings@cerner.com>
2019-09-21 10:22:37 -0500

    Simple HTTP server in Go

---
+++ http-servers.go
@@ -0,0 +1,26 @@
+// cerner_2^5_2019
+// Simple HTTP server in Go
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+	http.ListenAndServe(":8090", nil)
+}
