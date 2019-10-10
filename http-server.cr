8f63c045526a74727aa069fc51e6cc3a752bb374
Brad Jennings <brad.jennings@cerner.com>
2019-10-10 09:57:46 -0400

    HTTP server written in Crystal

---
+++ http-server.cr
@@ -0,0 +1,12 @@
+# cerner_2^5_2019
+# Basic HTTP server in Crystal
+require "http/server"
+
+server = HTTP::Server.new do |context|
+    context.response.content_type = "text/plain"
+    context.response.print "Hello world! The time is #{Time.local}"
+end
+
+address = server.bind_tcp 8080
+puts "Listening on http://#{address}"
+server.listen
